586aa3a91793043c61ad3247edd024b5289711ad
pandurangtiwari <119836434+pandurangtiwari@users.noreply.github.com>
2022-12-29 11:53:49 +0530

    Update SeedHelperTest.java

--- src/test/java/com/yelpcamp/seed/SeedHelperTest.java
+++ src/test/java/com/yelpcamp/seed/SeedHelperTest.java
@@ -71,22 +71,4 @@ public class SeedHelperTest {
         var user = userRepository.findByEmail("xyz' OR true --");
         System.out.println(user);
     }
-
-    @Test
-    @Disabled
-    public void getCspDirectives(){
-        final String SCRIPT_SRC = "script-src 'self' 'strict-dynamic' 'nonce-rAnd0m123' 'unsafe-inline' http: https: https://cdn.jsdelivr.net https://stackpath.bootstrapcdn.com/;";
-        final String STYLE_SRC = "style-src 'unsafe-inline' 'self' https://stackpath.bootstrapcdn.com;";
-        final String IMG_SRC = "img-src 'self' blob: data: https://res.cloudinary.com/dqbrojvar/ https://images.unsplash.com/;";
-        final String OBJECT_SRC = "object-src 'none';";
-        final String BASE_URI = "base-uri 'none';";
-        final String REQUIRE_TRUSTED_TYPES_FOR = "require-trusted-types-for 'script';";
-
-        List<String> cspDirectives = new ArrayList<>();
-        cspDirectives.add(SCRIPT_SRC); cspDirectives.add(STYLE_SRC); cspDirectives.add(IMG_SRC);
-        cspDirectives.add(OBJECT_SRC); cspDirectives.add(BASE_URI); cspDirectives.add(REQUIRE_TRUSTED_TYPES_FOR);
-
-        System.out.println(String.join("", cspDirectives));
-    }
-
 }
